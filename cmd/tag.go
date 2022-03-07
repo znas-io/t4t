761f5396f3504f4b55bc9e32019ecebc563ad8fc
Jose Sanz <znas@znas.io>
2022-03-07 10:28:26 -0700

    Added tags validation and path validations.

---
+++ cmd/tag.go
@@ -0,0 +1,59 @@
+package cmd
+
+/*
+Copyright © 2022 Jose Sanz <znas@znas.io>
+*/
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/znas-io/t4t/core"
+	"strings"
+)
+
+const (
+	short         = "Tags one or more files and directories to one of more tags"
+	long          = ``
+	validTagRegex = "^[a-zA-Z0-9-]+$"
+)
+
+// tagCmd represents the tag command
+var tagCmd = &cobra.Command{
+	Use:   "tag",
+	Short: short,
+	Long:  long,
+	Args:  cobra.MatchAll(cobra.MinimumNArgs(2), validateArgs),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("test")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(tagCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// tagCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	tagCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//tagCmd.MarkFlagCustom()
+}
+
+func validateArgs(_ *cobra.Command, args []string) error {
+	tags := strings.Split(args[0], ",")
+
+	var err error
+
+	if err = core.ValidateTagList(validTagRegex, tags...); err != nil {
+		return err
+	}
+
+	if err = core.ValidateFileAndDir(args[1:]...); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ core/validations.go
@@ -0,0 +1,53 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func ValidateTagList(regex string, tags ...string) error {
+	if len(tags) == 0 {
+		return errors.New("requires at least one tag")
+	}
+
+	for _, tag := range tags {
+		if tag == "" {
+			return errors.New("tags cannot be empty")
+		}
+
+		if match, err := regexp.MatchString(regex, tag); err != nil {
+			return err
+		} else if !match {
+			return errors.New(fmt.Sprintf("tag %v does not match %v", tag, regex))
+		}
+	}
+
+	return nil
+}
+
+func ValidateFileAndDir(paths ...string) error {
+	if len(paths) == 0 {
+		return errors.New("requires at least one file or directory to tag")
+	}
+
+	for _, path := range paths {
+		if path == "" {
+			return errors.New("path cannot be empty")
+		}
+
+		var file *os.File
+		var err error
+
+		if file, err = os.Open(path); err != nil {
+			return err
+		}
+
+		if err = file.Close(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
